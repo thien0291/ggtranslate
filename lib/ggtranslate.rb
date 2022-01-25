2a48b630115295463aa1ecd8287952ff766fdc68
Thien Tran <webmaster3t@gmail.com>
2022-01-25 11:32:49 +0700

    includes Exception & self class

--- lib/ggtranslate.rb
+++ lib/ggtranslate.rb
@@ -1,7 +1,15 @@
 require "ggtranslate/version"
 require_relative "ggtranslate/translator"
 require_relative "ggtranslate/result_object"
+require_relative "ggtranslate/languages"
 
 module Ggtranslate
-  class Error < StandardError; end
+  autoload :ApiCallException, "ggtranslate/exceptions"
+  autoload :ResponseParsingException, "ggtranslate/exceptions"
+  autoload :LANGUAGES, "ggtranslate/languages"
+
+  extend Translator
+
+  class << self
+  end
 end
